3bde0ba7419ae310faf3c80eebaee5c765602da2
mpaternostro <marcelodpaternostro@gmail.com>
2020-08-20 09:55:56 -0300

    Updates

---
+++ precache-manifest.7bda3e665357f860ffd5df3ede71cce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5203011692880283852e19a2c18ad2f",
+    "url": "/small-social-media-feed/index.html"
+  },
+  {
+    "revision": "b83349277eb9b3369022",
+    "url": "/small-social-media-feed/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "aece3e355ce6ecd11d35",
+    "url": "/small-social-media-feed/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "b83349277eb9b3369022",
+    "url": "/small-social-media-feed/static/js/2.d2433b11.chunk.js"
+  },
+  {
+    "revision": "5c4b00a641e6db9ec791fdc1d7ed67ad",
+    "url": "/small-social-media-feed/static/js/2.d2433b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aece3e355ce6ecd11d35",
+    "url": "/small-social-media-feed/static/js/main.c47618e3.chunk.js"
+  },
+  {
+    "revision": "468ec4269e0dc472e5b6",
+    "url": "/small-social-media-feed/static/js/runtime-main.4a716893.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/small-social-media-feed/precache-manifest.7bda3e665357f860ffd5df3ede71cce4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/small-social-media-feed/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
